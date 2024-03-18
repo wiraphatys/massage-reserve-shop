38699f7d1ef11f0d14fc4492bdbba070425fec5c
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-03-19 02:21:23 +0700

    Added sign-up sign-in page

--- frontend/src/components/SignInForm.tsx
+++ frontend/src/components/SignInForm.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+"use client"
+
+import config from '@/utils/config'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function SignInForm() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e: any) => {
+        try {
+            e.preventDefault()
+            const user = { email, password }
+            const response = await axios.post(`${config.api}/auth/login`, user, {withCredentials: true})
+            console.log("result", response.data)
+        } catch (e) {
+            console.log(e)
+        }
+    } 
+
   return (
       <div className="hero min-h-screen bg-emerald-300" >
           <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,19 +33,25 @@ function SignInForm() {
                           <label className="label">
                               <span className="label-text">Email</span>
                           </label>
-                          <input type="email" placeholder="email" className="input input-bordered" required />
+                          <input type="email" placeholder="email" className="input input-bordered" required 
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
                       </div>
                       <div className="form-control">
                           <label className="label">
                               <span className="label-text">Password</span>
                           </label>
-                          <input type="password" placeholder="password" className="input input-bordered" required />
+                          <input type="password" placeholder="password" className="input input-bordered" required 
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
                           <label className="label">
                               <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                           </label>
                       </div>
                       <div className="form-control mt-6">
-                          <button className="btn btn-outline btn-success">Sign In</button>
+                          <button className="btn btn-outline btn-success"
+                            onClick={(e) => handleSubmit(e)}
+                          >Sign In</button>
                       </div>
                   </form>
               </div>
--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -1,44 +1,80 @@
-import React from 'react'
+"use client"
 
-function SignUpForm() {
+import config from '@/utils/config'
+import axios from 'axios'
+import React, { useState } from 'react'
+
+function SignInForm() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [tel, setTel] = useState('')
+
+    const handleSubmit = async (e: any) => {
+        try {
+            e.preventDefault()
+            const payload = { 
+                name,
+                email,
+                password,
+                tel,
+                role: "user"
+             }
+            const response = await axios.post(`${config.api}/auth/register`, payload, { withCredentials: true })
+            console.log("result", response.data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <div className="hero min-h-screen bg-emerald-300">
+        <div className="hero min-h-screen bg-emerald-300" >
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Sign Up Now !</h1>
+                    <h1 className="text-5xl font-bold">Sign In now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form className="card-body">
                         <div className="form-control">
-                            <label className='label'>
-                                <span className='label-text'>Name</span>
+                            <label className="label">
+                                <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder='name' className='input input-bordered' required />
+                            <input type="text" placeholder="name" className="input input-bordered" required
+                                onChange={(e) => setName(e.target.value)}
+                            />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Telephone</span>
                             </label>
-                            <input type="text" placeholder="tel.0987654321" className="input input-bordered" required />
+                            <input type="text" placeholder="telephone" className="input input-bordered" required
+                                onChange={(e) => setTel(e.target.value)}
+                            />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" placeholder="email" className="input input-bordered" required
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" placeholder="password" className="input input-bordered" required
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-outline btn-success">Login</button>
+                            <button className="btn btn-outline btn-success"
+                                onClick={(e) => handleSubmit(e)}
+                            >Sign Up</button>
                         </div>
                     </form>
                 </div>
@@ -47,4 +83,4 @@ function SignUpForm() {
     )
 }
 
-export default SignUpForm
+export default SignInForm
