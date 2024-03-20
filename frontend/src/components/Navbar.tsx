5c2ffdeed1380e974e541f4ee3706c91b03b2c24
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-03-20 23:01:29 +0700

    Updated navbar to have dropdown menu setting and include profile menu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,10 +3,13 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Swal from 'sweetalert2'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Navbar() {
 
     const router = useRouter()
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const handleSignOut = () => {
 
@@ -34,9 +37,6 @@ function Navbar() {
             }
         })
     }
-    
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 
     useEffect(() => {
@@ -51,25 +51,31 @@ function Navbar() {
     return (
         <>
             <div className="navbar bg-emerald-900 text-gray-300 sticky top-0 z-50">
+
                 <div className="navbar-start">
+
+                    {/* mobile view */}
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-emerald-800 rounded-box w-52 font-bold">
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/massages'>Massages</a></li>
+                            {
+                                isLoggedIn ? (<li><a href='/reservations'>Reservation</a></li>)
+                                    : (<li><a href='/reservations' className='btn-disabled text-gray-400'>Reservation</a></li>)
+                            }
+                            <li><a>Contact</a></li>
+                            <li><a>About</a></li>
                         </ul>
                     </div>
+
+                    {/* MRS */}
                     <a className="btn btn-ghost text-xl" href='/'>MRS</a>
                 </div>
+
+                {/* desktop view */}
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li><a href='/'>Home</a></li>
@@ -82,14 +88,26 @@ function Navbar() {
                         <li><a>About</a></li>
                     </ul>
                 </div>
+
                 <div className="navbar-end">
                     {isLoggedIn && (
 
-                        <button className='btn btn-error btn-outline btn-sm'
-                            onClick={handleSignOut}
-                        >
-                            Log Out
-                        </button>
+                        // <button className='btn btn-error btn-outline btn-sm'
+                        //     onClick={handleSignOut}
+                        // >
+                        //     Log Out
+                        // </button>
+
+                        <div className="dropdown dropdown-end">
+                            <div>
+                                <button className='btn btn-ghost'><AccountCircleIcon fontSize="large" /></button>
+                            </div>
+                            <ul tabIndex={0} className={`menu dropdown-content z-[1] shadow bg-emerald-800 rounded-box w-40 mt-4 p-2`}>
+                                <li><a className='btn btn-success btn-sm text-gray-200 mb-2' href="#">Profile</a></li>
+                                <li><button className='btn btn-error btn-outline btn-sm' onClick={handleSignOut}>Logout</button></li>
+                            </ul>
+
+                        </div>
 
                     )}
                 </div>
@@ -99,17 +117,3 @@ function Navbar() {
 }
 
 export default Navbar
-
-
-{/* <div className="dropdown dropdown-end mr-2">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                            </div>
-                        </div>
-                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-gray-200 rounded-box w-30">
-                            <li><a>Profile</a></li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
-                    </div> */}
